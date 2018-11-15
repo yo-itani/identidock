d7fe1e8791892036bfbef47893fab5223ac22de6
yo itani <itani@bitstar.jp>
2018-11-15 20:48:08 +0900

    first commit

---
+++ cmd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+if [ "$ENV" = 'DEV' ]; then
+  echo "Running development server"
+  exec python "identidock.py"
+elif ["$ENV" = 'UNIT' ]; then
+  echo "Running Unit Tests"
+  exec python "tests.py"
+else
+  echo "Running production server"
+  exec uwsgi --http 0.0.0.0:9090 --wsgi-file identidock.py --callable app --stats 0.0.0.0:9191
+fi
