3ae7d62cc8a6a0d2720a2c95ce0f96b012b858c9
yo itani <itani@bitstar.jp>
2018-11-15 21:40:05 +0900

    パスを修正

--- cmd.sh
+++ cmd.sh
@@ -9,5 +9,5 @@ elif [ "$ENV" = 'UNIT' ]; then
   exec python "tests.py"
 else
   echo "Running production server"
-  exec uwsgi --http 0.0.0.0:9090 --wsgi-file identidock.py --callable app --stats 0.0.0.0:9191
+  exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
 fi
